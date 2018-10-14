0170107ce90a56b6e3f96452dcd425c1c44ca1c1
LindomarReitz <lindomar.reitz@gmail.com>
2018-10-14 18:47:12 +0100

    Update to .net core 2.1 and change to use xUnit

---
+++ APITestingDemo/CountryAPITest.cs
@@ -0,0 +1,96 @@
+﻿using RestSharp;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace APITestingDemo
+{
+    public class CountryAPITest
+    {
+        private RestClient client;
+
+        public CountryAPITest()
+        {
+            client = new RestClient("https://restcountries.eu/rest/v2");
+        }
+
+        [Fact]
+        public void CountryApi_FindByName_ReturnsOK()
+        {
+            var request = new RestRequest("/name/portugal");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+            string json = response.Content;
+
+            JArray countries = JArray.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal("Portugal", (string) countries[0]["name"]);
+        }
+
+        [Fact]
+        public void CoutryAPI_FindByCountryCode_ReturnsOK()
+        {
+            var request = new RestRequest("/alpha/de");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+            string json = response.Content;
+
+            JObject country = JObject.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal("Germany", (string) country["name"]);
+        }
+
+        [Fact]
+        public void CountryAPI_FindByCurrency_ReturnsOK()
+        {
+            var request = new RestRequest("/currency/eur");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+            string json = response.Content;
+
+            JArray countries = JArray.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal(36, countries.Count);    
+        }
+
+        [Fact]
+        public void CountryAPI_FindByCapital_ReturnsOK()
+        {
+            var request = new RestRequest("/capital/copenhagen");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+            string json = response.Content;
+
+            JArray countries = JArray.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal("Denmark", (string) countries[0]["name"]);
+        }
+
+        [Fact]
+        public void CountryAPI_FindByRegion_ReturnsOK()
+        {
+            var request = new RestRequest("/region/europe");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+            string json = response.Content;
+
+            JArray countries = JArray.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal(53, countries.Count);
+        }
+
+        [Fact]
+        public void CountryAPI_FindANonExistingCountry_ReturnsOK()
+        {
+            var request = new RestRequest("/name/xpto");
+
+            IRestResponse response = client.Execute(request, Method.GET);
+
+            Assert.Equal("NotFound", response.StatusCode.ToString());
+        }
+    }
+}
---
+++ APITestingDemo/XckdAPITest.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace APITestingDemo
+{
+    public class XckdAPITest
+    {
+        private RestClient client;
+        
+        public XckdAPITest()
+        {
+            client = new RestClient("http://xkcd.com");
+        }
+
+        [Fact]
+        public void XckdAPI_MakeASuccessfulRequest_ReturnsOK()
+        {
+            var request = new RestRequest("/info.0.json", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+        }
+
+        [Fact]
+        public void XckdAPI_MakeARequestForInexistentContent_ReturnsNotFound()
+        {
+            var request = new RestRequest("/xpto/info.0.json", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.Equal("NotFound", response.StatusCode.ToString());
+        }
+
+        [Fact]
+        public void XckdAPI_FindASpecificComic_ReturnsOK()
+        {
+            var request = new RestRequest("/1119/info.0.json", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            string json = response.Content;
+            JObject title = JObject.Parse(json);
+
+            Assert.Equal("OK", response.StatusCode.ToString());
+            Assert.Equal("Undoing", (string) title["title"]);
+        }
+
+        [Fact]
+        public void XckdAPI_FindAnInexistentComic_ReturnsNotFound()
+        {
+            var request = new RestRequest("/99999/info.0.json", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.Equal("NotFound", response.StatusCode.ToString());
+        }
+    }
+}
